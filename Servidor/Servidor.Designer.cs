c9863c3f2bb0294aff9f721dc836867555685a43
DellX17 <aaronaguilar12721@gmail.com>
2022-04-02 17:59:11 -0600

    Version 1 del servidor aaaaaa

---
+++ Servidor/Servidor.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace Servidor
+{
+    partial class Servidor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvInfo = new System.Windows.Forms.DataGridView();
+            this.btnTXT = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvInfo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvInfo
+            // 
+            this.dgvInfo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvInfo.Location = new System.Drawing.Point(12, 12);
+            this.dgvInfo.Name = "dgvInfo";
+            this.dgvInfo.RowTemplate.Height = 25;
+            this.dgvInfo.Size = new System.Drawing.Size(687, 392);
+            this.dgvInfo.TabIndex = 1;
+            // 
+            // btnTXT
+            // 
+            this.btnTXT.Location = new System.Drawing.Point(12, 423);
+            this.btnTXT.Name = "btnTXT";
+            this.btnTXT.Size = new System.Drawing.Size(192, 44);
+            this.btnTXT.TabIndex = 2;
+            this.btnTXT.Text = "Leer TXT";
+            this.btnTXT.UseVisualStyleBackColor = true;
+            this.btnTXT.Click += new System.EventHandler(this.btnTXT_Click_1);
+            // 
+            // Servidor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(711, 479);
+            this.Controls.Add(this.btnTXT);
+            this.Controls.Add(this.dgvInfo);
+            this.Name = "Servidor";
+            this.Text = "Servidor";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvInfo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvInfo;
+        private System.Windows.Forms.Button btnTXT;
+    }
+}
---
+++ Servidor/Servidor.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Servidor
+{
+    
+    public partial class Servidor : Form
+    {
+        DataTable dt = new DataTable();
+        public Servidor()
+        {
+            InitializeComponent();
+        }
+
+        private void leerTXT(string directorio)
+        {
+            using (StreamReader sr = new StreamReader(directorio))
+            {
+                List<string> datos = new List<string>();
+                string linea = string.Empty;
+                while ((linea=sr.ReadLine()) != null)
+                {
+                    datos.Add(linea);
+                }
+                usarTXT(datos);
+            }
+        }
+
+        private void usarTXT(List<string> datos)
+        {
+            if (!datos.Count.Equals(0))
+            {
+                int i = 0;
+                for (i = 0; i < datos.Count; i++)
+                {
+                    if (i.Equals(0))
+                    {
+                        string[] conceptos = datos[i].ToString().Split('%');
+                        dt.Columns.Add(conceptos[0]);
+                        dt.Columns.Add(conceptos[1]);
+                        dt.Columns.Add(conceptos[2]);
+                    }
+                    else
+                    {
+                        string[] conceptos = datos[i].ToString().Split('%');
+                        dt.Rows.Add(conceptos[0],conceptos[1],conceptos[2]);
+                    }
+                }
+                dgvInfo.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+                dgvInfo.DataSource = dt;
+            }
+        }
+
+
+        private string buscarArchivoTXT()
+        {
+            string nombreArchivo = string.Empty;
+            OpenFileDialog busqueda = new OpenFileDialog();
+            busqueda.Title = "Selecciona tu archivo TXT";
+            busqueda.Filter = "TXT Files (*.txt)|*.txt";
+            if (busqueda.ShowDialog().Equals(DialogResult.OK))
+            {
+                nombreArchivo = busqueda.FileName;
+            }return nombreArchivo;
+        }
+
+        private void btnTXT_Click_1(object sender, EventArgs e)
+        {
+            leerTXT(buscarArchivoTXT());
+        }
+    }
+}
